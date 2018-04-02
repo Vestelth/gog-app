f6da3f8603a9b4159969e708e2f5a5c1a5234870
Vestelth <vestelth@gmail.com>
2018-04-02 14:14:44 +0200

    app init

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainNavComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
